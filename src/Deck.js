546fa759b41fb8d00f8b257f6fdfb52b208a96cf
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-04-09 17:09:26 -0700

    implemented shuffle

--- src/Deck.js
+++ src/Deck.js
@@ -8,10 +8,12 @@ const Deck = () => {
   const [isDrawing, setIsDrawing] = useState(false);
   const [isShuffling, setIsShuffling] = useState(true);
 
+  const BASE_URL = "https://deckofcardsapi.com/api/deck"
+
   /** Get deck of cards when page loads */
   useEffect(() => {
     const getDeck = async () => {
-      let deckResp = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+      let deckResp = await axios.get(`${BASE_URL}/new/shuffle/?deck_count=1`);
       setDeckId(deckResp.data.deck_id);
       setIsShuffling(false);
     } 
@@ -21,7 +23,7 @@ const Deck = () => {
   const handleDraw = async () => {
     setIsDrawing(true);
     try {
-      let resp = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
+      let resp = await axios.get(`${BASE_URL}/${deckId}/draw/?count=1`);
       const {success, cards} = resp.data;
       if(success) {
         setDrawnCards(c => [...c, cards[0]]);
@@ -33,6 +35,17 @@ const Deck = () => {
     }
     setIsDrawing(false);
   }
+
+  const handleShuffle = async () => {
+    setIsShuffling(true);
+    try {
+      await axios.get(`${BASE_URL}/${deckId}/shuffle/`);
+      setDrawnCards([]);
+    } catch(e) {
+      console.log(e);
+    }
+    setIsShuffling(false);
+  }
   
   return (
     isShuffling 
@@ -42,6 +55,7 @@ const Deck = () => {
       ? <span>Drawing...</span>
       : <button onClick={handleDraw}>Draw a Card</button>
       }
+      <button onClick={handleShuffle}>Shuffle the Deck</button>
       {drawnCards.map(({code, image}) => {
         return <MemoizedCard key={code} imageUrl={image} />
       })}
